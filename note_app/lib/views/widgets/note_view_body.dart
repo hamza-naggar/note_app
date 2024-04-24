70182bd191217f25de7f57ed11d2545d2c938377
hamza <gghamzg17@gmail.com>
2024-04-24 11:32:38 +0300

    create note list view

--- note_app/lib/views/widgets/note_view_body.dart
+++ note_app/lib/views/widgets/note_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/custom_note_item.dart';
+import 'package:note_app/views/widgets/note_list_view.dart';
 
 class NotesViewsBody extends StatelessWidget {
   const NotesViewsBody({super.key});
@@ -14,63 +16,11 @@ class NotesViewsBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          Expanded(child:NotesListView()),
         ],
       ),
-    );
+    ); 
   }
 }
 
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24 , bottom: 24 ,left: 16),
-      decoration: BoxDecoration(
-          color: Color(0xffffcc80), borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutter tips',
-              style: TextStyle(
-                color: Colors.black,
-               fontSize: 26
-               ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text(
-                'Build your career with hamza najjar',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontSize: 18
-                  ),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 24,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May21 , 2022',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
