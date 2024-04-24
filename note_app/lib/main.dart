dd271a68b9076c5c76fb0c8d0b7238c163f00b1b
hamza <gghamzg17@gmail.com>
2024-04-24 11:15:13 +0300

    create note widget

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -12,8 +12,10 @@ class noteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme : ThemeData(
-        brightness: Brightness.dark
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins'
       ),
+      
       home: const NotesViews(),
     );
   }
--- note_app/lib/views/widgets/note_view_body.dart
+++ note_app/lib/views/widgets/note_view_body.dart
@@ -9,9 +9,66 @@ class NotesViewsBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children:const [
-          SizedBox(height: 50,),
-          CustomAppBar()
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24 , bottom: 24 ,left: 16),
+      decoration: BoxDecoration(
+          color: Color(0xffffcc80), borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(
+                color: Colors.black,
+               fontSize: 26
+               ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16,bottom: 16),
+              child: Text(
+                'Build your career with hamza najjar',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 18
+                  ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May21 , 2022',
+              style: TextStyle(
+                color: Colors.black.withOpacity(.4),
+                fontSize: 16
+              ),
+            ),
+          )
         ],
       ),
     );
