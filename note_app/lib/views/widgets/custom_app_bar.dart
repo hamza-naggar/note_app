47a3266746a7f4c4bc00c9fce6ef6ea3c7b19dc8
hamza <gghamzg17@gmail.com>
2024-04-25 10:29:30 +0300

    edit nore view

--- note_app/lib/views/widgets/custom_app_bar.dart
+++ note_app/lib/views/widgets/custom_app_bar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-   CustomAppBar({super.key,required this.title,required this.icon});
+   const CustomAppBar({super.key,required this.title,required this.icon});
    final IconData icon;
   final String title;
   @override
--- note_app/lib/views/widgets/edit_note_view_body.dart
+++ note_app/lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/custom_app_bar.dart';
+import 'package:note_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -8,7 +9,7 @@ class EditNoteViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
+      child: const Column(
         children: [
           SizedBox(
             height: 50,
@@ -17,6 +18,11 @@ class EditNoteViewBody extends StatelessWidget {
           title: 'Notes'
           ,icon: Icons.search,
           ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height:16 ,),
+          CustomTextField(
+          hint: 'Content',
+          maxLines: 5,)
       ],),
     );
   }
